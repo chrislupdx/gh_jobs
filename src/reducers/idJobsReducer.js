f2e454088d8e5376ae3b3f1e27e259b0b12fd0aa
dreamer <wildetangeant@gmail.com>
2019-06-05 09:32:55 -0700

    id reducer written

---
+++ src/reducers/idJobsReducer.js
@@ -0,0 +1,21 @@
+import {
+  FETCH_JOBBYID,
+  FETCH_JOBBYID_LOADING,
+} from '../actions/idSearchActions';
+
+const initialState = {
+  jobapp: null,
+  loading: true,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_JOBBYID:
+      return { ...initialState, jobapp: action.payload, loading: false };
+    case FETCH_JOBBYID_LOADING:
+      return { ...state, loading: true };
+    default:
+      return state;
+  }
+}
