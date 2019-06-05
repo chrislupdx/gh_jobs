d2b60e9782b800886031b63821e196a86677c542
dreamer <wildetangeant@gmail.com>
2019-06-05 09:37:55 -0700

    jobbyid added to action reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import pdxjobs from './pdxJobsReducer';
+import jobsbyid from './idJobsReducer';
 
 export default combineReducers({
-  pdxjobs
+  pdxjobs,
+  jobsbyid
 });
