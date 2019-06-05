d705f44ee64a563efe2832c5a9d0f9ff5314e33b
dreamer <wildetangeant@gmail.com>
2019-06-05 09:28:26 -0700

    by id action written

---
+++ src/actions/idSearchActions.js
@@ -0,0 +1,10 @@
+import { getJobById } from '../service/ghApi';
+import { createAction } from 'promise-middleware-redux';
+
+export const [
+  fetchJobById,
+  FETCH_JOBBYID,
+  FETCH_JOBBYID_LOADING,
+  _,
+  FETCH_JOBBYID_ERROR,
+] = createAction('FETCH_JOBBYID', getJobById);
