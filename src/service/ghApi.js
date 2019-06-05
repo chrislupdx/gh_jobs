1150acafa7ebb56eea1eb58066402c6b58d5c177
dreamer <wildetangeant@gmail.com>
2019-06-05 09:22:41 -0700

    byid service written

--- src/service/ghApi.js
+++ src/service/ghApi.js
@@ -11,3 +11,7 @@ export const getGenPdxJobs = () => {
   return get('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?location=Portland+OR');
    
 };
+
+export const getJobById = id => {
+  return get(`https://jobs.github.com/positions/${id}.json`);
+};
