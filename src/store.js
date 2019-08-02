ae36e8972d60274e4af6c43c793e33b25d162a91
dreamer <wildetangeant@gmail.com>
2019-08-01 17:37:15 -0700

    got rid of redux dvtools

--- src/store.js
+++ src/store.js
@@ -6,11 +6,11 @@ import {
 import reducer from './reducers';
 import { promiseMiddleware } from 'promise-middleware-redux';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default createStore(
   reducer,
-  compose(
-    applyMiddleware(promiseMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(promiseMiddleware)
   )
-)
-;
+);
